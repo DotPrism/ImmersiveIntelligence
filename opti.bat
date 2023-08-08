57545dafcfb684603db4145055ed7ed635fed1de
VDeltaGabriel <59255191+VDeltaGabriel@users.noreply.github.com>
2023-08-08 21:46:49 +0200

    Workflow textures & audio compression

--- opti.bat
+++ opti.bat
@@ -1,4 +1,5 @@
 @echo off
 echo.
-set apppath="optipng.exe"
-for /R "src\main\resources\assets\immersiveintelligence\textures" %%f in (*.png) do %apppath% %%f -zc9 -zm9 -zs3 -f0-5 -nc -o7 -strip "all"
+set optipng="oxipng.exe"
+
+for /R "src\main\resources\assets\immersiveintelligence\textures" %%f in (*.png) do %optipng% %%f --zc 9 -f 0-5 --nc --strip all
